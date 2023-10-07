aefe47652de653d9fb19d3134bef350c63ced8eb
cobrianz <briancheruiyot.h@gmail.com>
2023-10-07 17:10:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/* template_l40xzeq
+
+service_ic6e6mk
+
+N_zB7QJt4YSPXyryW */
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList+= ' modal__overlay--visible';
+    emailjs.send('service_ic6e6mk', 'template_l40xzeq', event.target,'N_zB7QJt4YSPXyryW' )
+    .then(() => {
+        loading.classList.remove('modal__overlay--visible');
+        success.classList+= ' modal__overlay--visible';
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible');
+        alert("Email server temporarily unavailable. please contact me through briancheruiyot022@gmail.com");
+    });
+
+    setTimeout(() => {
+    
+        console.log('success!')
+    }, 1000);
+
+} 
